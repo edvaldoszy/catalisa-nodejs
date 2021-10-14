6fc2acc5a7969ba3a1c15fc7f5a97f4362259e82
Edvaldo Szymonek <edvaldoszy@gmail.com>
2021-10-13 23:01:21 -0300

    Cria rota de tarefas

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const modelos = require('./modelos');
 
 const app = express();
 
 app.get('/catalisa', (request, response) => {
   response.sendFile('./catalisa.html', { root: './public' });
+});
+
+app.get('/tarefas', (request, response) => {
+  modelos.Tarefa
+    .fetchAll()
+    .then(tarefas => {
+      response.json(tarefas);
+    });
 });
 
 exports.app = app;
